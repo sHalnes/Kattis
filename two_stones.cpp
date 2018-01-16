86ceafa792158969a79a1b17a22fc7d37592aac7
Svetlana Halnes <sHalnes@users.noreply.github.com>
2018-01-16 21:07:36 +0100

    two stones

---
+++ two_stones.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int a;
+    std::cin >> a;
+    if (a % 2 > 0){
+        std::cout<<"Alice";
+    }
+    else {
+        std::cout<<"Bob";
+    }
+    return 0;
+}
+
